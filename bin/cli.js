17366aa83c7e4cef05dd3a1f7d203af779f47ce6
saseungmin <dbd02169@naver.com>
2022-07-05 23:55:18 +0900

    [Update] Cli and README.md를 업데이트하라
    - bin/cli.js를 수정
    - Update README.md

--- bin/cli.js
+++ bin/cli.js
@@ -6,16 +6,48 @@ const path = require('path');
 const fs = require('fs');
 const packageJson = require('../package.json');
 
+const colors = {
+  reset: '\x1b[0m',
+  bright: '\x1b[1m',
+  dim: '\x1b[2m',
+  underscore: '\x1b[4m',
+  blink: '\x1b[5m',
+  reverse: '\x1b[7m',
+  hidden: '\x1b[8m',
+  fg: {
+    black: '\x1b[30m',
+    red: '\x1b[31m',
+    green: '\x1b[32m',
+    yellow: '\x1b[33m',
+    blue: '\x1b[34m',
+    magenta: '\x1b[35m',
+    cyan: '\x1b[36m',
+    white: '\x1b[37m',
+    crimson: '\x1b[38m', // Scarlet
+  },
+  bg: {
+    black: '\x1b[40m',
+    red: '\x1b[41m',
+    green: '\x1b[42m',
+    yellow: '\x1b[43m',
+    blue: '\x1b[44m',
+    magenta: '\x1b[45m',
+    cyan: '\x1b[46m',
+    white: '\x1b[47m',
+    crimson: '\x1b[48m',
+  },
+};
+
 if (process.argv.length < 3) {
-  console.log('\x1b[31m%s\x1b[0m', '========================= 🚨 E R R O R 🚨 =========================');
-  console.log('\x1b[31m%s\x1b[0m', '🚨 Warning:');
+  console.log(`${colors.fg.red}%s${colors.reset}`, '========================= 🚨 E R R O R 🚨 =========================');
+  console.log(`${colors.fg.red}%s${colors.reset}`, '🚨 Warning:');
   console.log('');
   console.log('Please specify the project directory:');
-  console.log('\x1b[32m%s\x1b[0m', 'npx @harang-jennie/nextjs-starter <project-directory>');
+  console.log(`${colors.fg.green}npx @harang-jennie/nextjs-starter${colors.reset}`, `${colors.fg.magenta}<my-nextjs-app>${colors.reset}`);
   console.log('');
   console.log('For example:');
-  console.log('npx @harang-jennie/nextjs-starter my-nextjs-project');
-  console.log('\x1b[31m%s\x1b[0m', '=========================== 🚨 E N D 🚨 ===========================');
+  console.log(`${colors.fg.cyan}npx @harang-jennie/nextjs-starter${colors.reset}`, `${colors.fg.magenta}my-nextjs-app${colors.reset}`);
+  console.log(`${colors.fg.red}%s${colors.reset}`, '=========================== 🚨 E N D 🚨 ===========================');
 
   process.exit(1);
 }
@@ -33,31 +65,28 @@ if (projectName !== '.') {
     fs.mkdirSync(projectPath);
   } catch (err) {
     if (err.code === 'EEXIST') {
-      console.log('\x1b[31m%s\x1b[0m', '========================= 🚨 E R R O R 🚨 =========================');
-      console.log(projectName);
+      console.log(`${colors.fg.red}%s${colors.reset}`, '🚨 Something Wrong... 🚨');
       console.log(
-        '\x1b[31m%s\x1b[0m',
+        `${colors.fg.red}%s${colors.reset}`,
         `🚨 The folder ${projectName} already exist in the current directory, please give it another name.`,
       );
-      console.log('\x1b[31m%s\x1b[0m', '=========================== 🚨 E N D 🚨 ===========================');
     } else {
-      console.log('\x1b[31m%s\x1b[0m', '========================= 🚨 E R R O R 🚨 =========================');
-      console.log(err);
-      console.log('\x1b[31m%s\x1b[0m', '=========================== 🚨 E N D 🚨 ===========================');
+      console.log(`${colors.fg.red}%s${colors.reset}`, '🚨 Something Wrong... 🚨');
+      console.log(`${colors.fg.red}%s${colors.reset}`, `${err}`);
     }
     process.exit(1);
   }
 }
 
-async function main() {
+async function run() {
   try {
     console.log('========================= 🚀 S T A R T 🚀 =========================');
-    console.log('Using yarn berry with zero install.');
-    console.log("This next.js starter kit doesn't need to install dependencies.");
+    console.log(`${colors.fg.cyan}%s${colors.reset}`, '👉 Using yarn berry with zero install.');
+    console.log(`${colors.fg.yellow}%s${colors.reset}`, "This next.js starter kit doesn't need to install dependencies.");
     console.log('https://yarnpkg.com/features/zero-installs');
     console.log('');
     console.log('⭐ Next.js Starter Kit ⭐');
-    console.log('🥰 Create by Harang');
+    console.log(`🥰 Create by ${packageJson.author}`);
     console.log(`🚀 From ${GIT_REPOSITORY}`);
     console.log(`🏷️ Version: ${packageJson.version}`);
     console.log('');
@@ -77,15 +106,15 @@ async function main() {
     console.log('🔥 Removing useless files:');
     execSync('npx rimraf ./.git');
     console.log('');
-    console.log('\x1b[36m%s\x1b[0m', 'Successfully installed!');
+    console.log(`${colors.fg.cyan}%s${colors.reset}`, 'Successfully installed!');
     console.log('');
-    console.log('\x1b[35m%s\x1b[0m', '🎉 The installation is done, ready to use. Happy coding!');
+    console.log(`${colors.fg.magenta}%s${colors.reset}`, '🎉 The installation is done, ready to use.');
+    console.log(`${colors.fg.magenta}%s${colors.reset}`, `Go to cd ./${projectName}`);
     console.log('========================= 🎉 E N D 🎉 =========================');
   } catch (error) {
-    console.log('\x1b[31m%s\x1b[0m', '========================= 🚨 E R R O R 🚨 =========================');
-    console.log(error);
-    console.log('\x1b[31m%s\x1b[0m', '=========================== 🚨 E N D 🚨 ===========================');
+    console.log(`${colors.fg.red}%s${colors.reset}`, '🚨 Something Wrong... 🚨');
+    console.log(`${colors.fg.red}%s${colors.reset}`, `${error}`);
   }
 }
 
-main();
+run();
