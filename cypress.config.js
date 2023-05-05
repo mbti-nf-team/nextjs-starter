c1bb2e2da01b4e03e25ed9b4d3959dac3c20e84d
saseungmin <dbd02169@naver.com>
2023-05-06 00:12:09 +0900

    chore(dependency): update nextjs dependency
    - update nextjs dependency to v13.4.1
    - nextjs app router 세팅
    - remove emotion dependency
    - update readme
    - update yarn version

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // set your project id: https://cloud.cypress.io/
-  projectId: 'qh6iij',
+  projectId: process.env.CYPRESS_PROJECT_ID,
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
